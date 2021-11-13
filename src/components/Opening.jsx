6c5fe48cde5ca733951262ee4ca14a52d60050e2
jhonorozmx <Jhonattan.Orozco@theksquaregroup.com>
2021-11-13 03:58:11 -0600

    Improved styles and animations

--- src/components/Opening.jsx
+++ src/components/Opening.jsx
@@ -4,26 +4,30 @@ import swlogo from "../assets/images/sw-logo.png";
 
 const Opening = ({ modalContent, modalHandler }) => {
   const [animate, setAnimate] = useState("animated");
+  const [textVisibility, setTextVisibility] = useState("hidden");
   const { episode, intro, title } = modalContent;
 
-  const animationHandler = (state) => {
-    setAnimate(state);
+  const skipHandler = () => {
+    setAnimate("");
+    setTextVisibility("visible");
+  };
+
+  const replayHandler = () => {
+    setTextVisibility("hidden");
+    setAnimate("");
+    setTimeout(setAnimate, 0, "animated");
   };
 
   return (
     <div className="modal-container">
       <div className="modal-top">
-        <button
-          id="skip"
-          className="small-btn"
-          onClick={() => animationHandler("")}
-        >
+        <button id="skip" className="small-btn" onClick={() => skipHandler()}>
           Skip
         </button>
         <button
           id="replay"
           className="small-btn"
-          onClick={() => animationHandler("animated")}
+          onClick={() => replayHandler()}
         >
           <i className="fas fa-redo 2x"></i>
         </button>
@@ -39,14 +43,16 @@ const Opening = ({ modalContent, modalHandler }) => {
         <div className="modal-content">
           <div className="front">
             <h1 className={`intro ${animate}`}>
-              A long time ago in a galaxy far, far away...
+              A long time ago, in a galaxy far, far away...
             </h1>
             <img
               className={`swlogo ${animate}`}
               src={swlogo}
               alt="star wars logo"
             />
-            <p className={`op-text ${animate}`}>{intro}</p>
+            <div className={`op-text ${textVisibility} ${animate} `}>
+              <p>{intro}</p>
+            </div>
           </div>
           <div className="crawl">
             <div className={`crawl-text ${animate}`}>
